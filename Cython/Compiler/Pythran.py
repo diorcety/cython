eb5411fdd6748a44875045b0122d3725d9369cbb
Yann Diorcet <yann.diorcet@xenocs.com>
2020-08-18 12:06:25 +0200

    Support local pythran version

--- Cython/Compiler/Pythran.py
+++ Cython/Compiler/Pythran.py
@@ -5,12 +5,49 @@ from __future__ import absolute_import
 from .PyrexTypes import CType, CTypedefType, CStructOrUnionType
 
 import cython
+import re
+
+VERSION_PATTERN = r"""
+    v?
+    (?:
+        (?:(?P<epoch>[0-9]+)!)?                           # epoch
+        (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
+        (?P<pre>                                          # pre-release
+            [-_\.]?
+            (?P<pre_l>(a|b|c|rc|alpha|beta|pre|preview))
+            [-_\.]?
+            (?P<pre_n>[0-9]+)?
+        )?
+        (?P<post>                                         # post release
+            (?:-(?P<post_n1>[0-9]+))
+            |
+            (?:
+                [-_\.]?
+                (?P<post_l>post|rev|r)
+                [-_\.]?
+                (?P<post_n2>[0-9]+)?
+            )
+        )?
+        (?P<dev>                                          # dev release
+            [-_\.]?
+            (?P<dev_l>dev)
+            [-_\.]?
+            (?P<dev_n>[0-9]+)?
+        )?
+    )
+    (?:\+(?P<local>[a-z0-9]+(?:[-_\.][a-z0-9]+)*))?       # local version
+"""
+version_regex = re.compile(r"^\s*" + VERSION_PATTERN + r"\s*$", re.VERBOSE | re.IGNORECASE)
 
 try:
     import pythran
-    pythran_is_pre_0_9 = tuple(map(int, pythran.__version__.split('.')[0:2])) < (0, 9)
-    pythran_is_pre_0_9_6 = tuple(map(int, pythran.__version__.split('.')[0:3])) < (0, 9, 6)
-except ImportError:
+    pythran_version = version_regex.search(pythran.__version__)
+    if not pythran_version:
+        raise InvalidVersion("Invalid pythran version: '{0}'".format(pythran.__version__))
+    pythran_release = tuple(int(i) for i in pythran_version.group("release").split("."))
+    pythran_is_pre_0_9 = pythran_release[0:2] < (0, 9)
+    pythran_is_pre_0_9_6 = pythran_release[0:3] < (0, 9, 6)
+except (ImportError, AttributeError):
     pythran = None
     pythran_is_pre_0_9 = True
     pythran_is_pre_0_9_6 = True
